6a562023b44d2723e2c82109d1ce87869bfdef79
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-15 18:44:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 board = ["X"," ","O"," ","X"," "," "," "," ",]
 def turn_count(board)
 counter = 0
-board.each do |index|
-  if board[index] = "X" || index = "O"
+board.each do |turn|
+  if turn == "X" || turn == "O"
     counter += 1
     puts "Number of turns remaining: #{counter}"
-    return counter
     end
   end
+  return counter
 end
