155ac345412b36a87aa56a61110cd5a1af6effd2
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-15 18:31:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
-board = [" "," "," "," "," "," "," "," "," ",]
-board.each do |turn|
-  puts "#{turn.length}"
+counter = 0
+if board[counter] != " "
+  counter -= 1
+  puts "#{board[counter]}"
   end
 end
